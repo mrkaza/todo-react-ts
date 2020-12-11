18585a04f87a496b23e8cdcbb6a9969797e8412e
mrkaza <marko.mrkonjic97@gmail.com>
2020-12-11 12:57:29 +0100

    todoReducer fixed

--- src/consts/rootReducer.tsx
+++ src/consts/rootReducer.tsx
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 import { authReducer } from "modules/auth";
-import { todoReducer } from "../modules/todo/redux";
+import { todoReducer } from "modules/todo";
 
 export const rootReducer = combineReducers({
   auth: authReducer,
--- src/modules/todo/index.ts
+++ src/modules/todo/index.ts
@@ -1,4 +1,4 @@
-export * from "./components";
 export * from "./redux";
+export * from "./components";
 export * from "./models";
 export * from "./services";
