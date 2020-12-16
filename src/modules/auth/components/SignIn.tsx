eeaea91ebabaee9513edd2e8f550df45fe89dd28
mrkaza <marko.mrkonjic97@gmail.com>
2020-12-16 09:23:34 +0100

    added react-hook-form to login page

--- src/modules/auth/components/SignIn.tsx
+++ src/modules/auth/components/SignIn.tsx
@@ -2,30 +2,38 @@ import { Button, Input } from 'components';
 import { RootStore } from 'consts';
 import { login } from 'modules/auth';
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { Facebook } from './Facebook';
 
 export const SignIn: React.FC = () => {
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+  // const [email, setEmail] = useState<string>('');
+  // const [password, setPassword] = useState<string>('');
   const dispatch = useDispatch();
 
   const errorMessage: null | string = useSelector(
     (state: RootStore) => state.auth.loginError,
   );
 
-  const handleSubmit = (e: React.SyntheticEvent) => {
-    e.preventDefault();
-    dispatch(login(email, password));
-    setEmail('');
-    setPassword('');
-  };
+  const { register, handleSubmit, errors } = useForm();
 
+  // const handleSubmit = (e: React.SyntheticEvent) => {
+  //   e.preventDefault();
+  //   dispatch(login(email, password));
+  //   // setEmail('');
+  //   // setPassword('');
+  // };
+
+  const onSubmit = (data: any) => {
+    dispatch(login(data.email, data.password));
+    console.log(data);
+  };
+  console.log(errors);
   return (
     <div>
-      <form className="col s12" onSubmit={handleSubmit}>
-        <Input
+      <form className="col s12" onSubmit={handleSubmit(onSubmit)}>
+        {/* <Input
           value={email}
           id="email"
           type="email"
@@ -42,7 +50,32 @@ export const SignIn: React.FC = () => {
           onChange={(e) => setPassword(e.target.value)}
           htmlFor="password"
           label="Password"
+        /> */}
+        <input
+          name="email"
+          ref={register({
+            required: { value: true, message: 'This field is required' },
+            pattern: {
+              value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+              message: 'Email is not formed correctly',
+            },
+          })}
         />
+        {errors.email && <p className="red-text">{errors.email.message}</p>}
+        <input
+          type="password"
+          name="password"
+          ref={register({
+            required: { value: true, message: 'This field is required' },
+            minLength: {
+              value: 8,
+              message: 'Minimal lenght of password is 8 char.',
+            },
+          })}
+        />
+        {errors.password && (
+          <p className="red-text">{errors.password.message}</p>
+        )}
         <div className="facebook col s12">
           <Button
             className="btn waves-effect waves-light"
