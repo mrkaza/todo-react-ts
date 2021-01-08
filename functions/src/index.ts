57493adf92f327e883c55a5fe45655e2607820e2
mrkaza <marko.mrkonjic97@gmail.com>
2021-01-08 12:14:58 +0100

    functions folder organized

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,48 +1,5 @@
-import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
 admin.initializeApp();
 
-export const todoCount = functions.firestore
-  .document('todos/{todoId}')
-  .onCreate(async () => {
-    let todoCount: number;
-    let todoCompleted: number;
-    const statusRef = admin.firestore().collection('todoStatus').doc('status');
-
-    const doc = await statusRef.get();
-    if (doc.exists) {
-      todoCount = doc.data()?.count;
-      todoCompleted = doc.data()?.completed;
-    } else {
-      todoCount = 0;
-      todoCompleted = 0;
-
-      statusRef.set({
-        count: 0,
-        completed: 0,
-      });
-    }
-
-    return statusRef.set(
-      {
-        count: todoCount + 1,
-        completed: todoCompleted,
-      },
-      { merge: true },
-    );
-  });
-
-export const deleteCount = functions.firestore
-  .document('todos/{completed}')
-  .onUpdate(async () => {
-    let todoCompleted: number;
-    const statusRef = admin.firestore().collection('todoStatus').doc('status');
-
-    const doc = await statusRef.get();
-    todoCompleted = doc.data()?.completed;
-
-    return await statusRef.update({
-      completed: todoCompleted + 1,
-    });
-  });
+export * from './todo';
---
+++ functions/src/todo/todoCounter.ts
@@ -0,0 +1,47 @@
+import * as functions from 'firebase-functions';
+
+import admin = require('firebase-admin');
+
+export const todoCount = functions.firestore
+  .document('todos/{todoId}')
+  .onCreate(async () => {
+    let todoCount: number;
+    let todoCompleted: number;
+    const statusRef = admin.firestore().collection('todoStatus').doc('status');
+
+    const doc = await statusRef.get();
+    if (doc.exists) {
+      todoCount = doc.data()?.count;
+      todoCompleted = doc.data()?.completed;
+    } else {
+      todoCount = 0;
+      todoCompleted = 0;
+
+      statusRef.set({
+        count: 0,
+        completed: 0,
+      });
+    }
+
+    return statusRef.set(
+      {
+        count: todoCount + 1,
+        completed: todoCompleted,
+      },
+      { merge: true },
+    );
+  });
+
+export const deleteCount = functions.firestore
+  .document('todos/{completed}')
+  .onUpdate(async () => {
+    let todoCompleted: number;
+    const statusRef = admin.firestore().collection('todoStatus').doc('status');
+
+    const doc = await statusRef.get();
+    todoCompleted = doc.data()?.completed;
+
+    return await statusRef.update({
+      completed: todoCompleted + 1,
+    });
+  });
