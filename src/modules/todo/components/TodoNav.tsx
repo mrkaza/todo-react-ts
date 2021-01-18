047133f2e193f971f7cd2825d478b14dd19c03e9
mrkaza <marko.mrkonjic97@gmail.com>
2021-01-18 11:12:58 +0100

    added logout to todo nav

--- src/modules/todo/components/TodoNav.tsx
+++ src/modules/todo/components/TodoNav.tsx
@@ -1,18 +1,34 @@
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Button } from 'components';
+import { logout } from 'modules/auth';
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 import TodoForm from './TodoForm';
 
 export const TodoNav: React.FC = () => {
   const [modal, setModal] = useState(false);
+  const [profile, setProfile] = useState(false);
+  const dispatch = useDispatch();
 
   const showModal = () => {
     setModal(true);
   };
 
-  console.log(modal);
+  const showProfile = () => {
+    setProfile(true);
+  };
+
+  const hideProfile = () => {
+    setProfile(false);
+  };
+
+  const signOut = () => {
+    dispatch(logout());
+  };
+
+  console.log(profile);
 
   return (
     <>
@@ -23,9 +39,25 @@ export const TodoNav: React.FC = () => {
         >
           Add todo
         </Button>
-        <Button className="button  button--circle button--circle--small button--outline-primary button--text button--light">
+        <Button
+          onMouseOver={showProfile}
+          className="button  button--circle button--circle--small button--outline-primary button--text button--light"
+        >
           <FontAwesomeIcon icon={faUser} size="sm" />
         </Button>
+        <div
+          onMouseLeave={hideProfile}
+          className={`todo__nav__profile ${
+            profile ? 'todo__nav__profile--active' : ''
+          }`}
+        >
+          <Button
+            onClick={signOut}
+            className="button button--text button--light"
+          >
+            Logout
+          </Button>
+        </div>
       </div>
 
       <div
